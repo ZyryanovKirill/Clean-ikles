8e4bcaf990236752e6d2f8bf7bbe55dbb6da3997
ZyryanovKirill <117571742+ZyryanovKirill@users.noreply.github.com>
2023-03-18 10:06:39 +0600

    Add files via upload

---
+++ Clean-ikles/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        speed: 1000,
+        infinite: true,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+});
+
+$(document).ready(function(){
+    $('.slider-2').slick({
+        arrows: true,
+        dots: true,
+        speed: 1000,
+        infinite: true,
+        slidesToShow: 1,
+        variableWidth: true,
+    });
+});
+
+$(document).ready(function(){
+    $('.slider-3').slick({
+        arrows: true,
+        dots: true,
+        speed: 1000,
+        infinite: true,
+        slidesToShow: 1,
+    });
+});
